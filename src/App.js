1205cf4d0b1f778d191fbede34f235b260fe355c
VVE12 <vasilievvladislav12@gmail.com>
2019-01-12 17:39:07 +0300

    Another Commit

--- src/App.js
+++ src/App.js
@@ -3,29 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
+
 import styled from 'styled-components'
+
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 49px;
+    background-color: #242424;
+    opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+    height: 89px;
+    padding-top: 20px;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <SayFullName name="Ivan" surname="Petrychenko" link="vk.com" />
-        <SayFullName name="Anna" surname="Kamneva" link="facebook.com" />
-        <SayFullName name="Iv" surname="Petrychenko" link="#" />
+      <div className="App">
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper>
+        <Main />
       </div>
     );
   }
-}
-
-function SayFullName(props) {
-  return (
-    <div>
-      <h1>Моё имя {props.name}, фамилия - {props.surname}</h1>
-      <a href={props.link}>Ссылка на мой профиль</a>
-    </div>
-  )
 }
 
 export default App;
