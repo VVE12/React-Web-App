a65609c5e1b801f441d2fc6419ad8c4a014463b1
VVE12 <vasilievvladislav12@gmail.com>
2019-01-13 19:44:02 +0300

    Last Commit

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -9,7 +9,7 @@ class Slider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      slide: [
+      slides: [
         {
           eachSlide: `url(${Slide_1})`
         },
@@ -29,7 +29,7 @@ class Slider extends React.Component {
     }
     this.state.max = this.state.slides.length;
     this.intervalBetweenSlides = this.intervalBetweenSlides.bind(this);
-    this.toggleAutoplay = this.toggleAutoplay.bind(this);
+    this.toggleAutoPlay = this.toggleAutoPlay.bind(this);
     this.nextOne = this.nextOne.bind(this);
     this.prevOne = this.prevOne.bind(this);
   }
@@ -55,7 +55,7 @@ class Slider extends React.Component {
     }
   }
 
-  toggleAutoplay() {
+  toggleAutoPlay() {
     this.setState ({
       autoplay: !this.state.autoplay
     })
@@ -72,7 +72,7 @@ class Slider extends React.Component {
   }
 
   prevOne() {
-    (this.satte.active > 0) ?
+    (this.state.active > 0) ?
     this.setState ({
       active: this.state.active - 1
     }) :
@@ -130,15 +130,15 @@ class Slider extends React.Component {
     let playStop;
 
     if(this.state.autoplay) {
-      playStop = <svg fill = '#FFFFFF' height = '24' viewBox = '0 0 24 24' width = '24'>
-                    <path d='M0 0h24v24H0z' fill = 'none'/>
+      playStop = <svg fill='#FFFFFF' height='24' viewBox='0 0 24 24' width='24'>
+                    <path d='M0 0h24v24H0z' fill='none'/>
                     <path d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14H9V8h2v8zm4 0h-2V8h2v8z'/>
-                 </svg>;
+                 </svg>
     } else {
-      playStop = <svg fill = '#FFFFFF' height = '24' viewBox = '0 0 24 24' width = '24'>
-                    <path d='M0 0h24v24H0z' fill = 'none'/>
+      playStop = <svg fill='#FFFFFF' height='24' viewBox='0 0 24 24' width='24'>
+                    <path d="M0 0h24v24H0z" fill="none"/>
                     <path d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z'/>
-                 </svg>;
+                 </svg>
     }
     return playStop
   }
@@ -150,19 +150,19 @@ class Slider extends React.Component {
             type="button"
             className="arrows prev"
             onClick={this.prevOne}>
-            <svg fill = '#FFFFFF' width = '50' height = '50' viewBox = '0 0 24 24'>
-                <path d='M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z'/>
-                <path d='M0 0h24v24H0z' fill = 'none'/>
-            </svg>;
+            <svg fill='#FFFFFF' width='50' height='50' viewBox='0 0 24 24'>
+	             <path d='M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z'/>
+	              <path d='M0 0h24v24H0z' fill='none'/>
+            </svg>
         </button>
         <button
             type="button"
             className="arrows next"
             onClick={this.nextOne}>
-            <svg fill = '#FFFFFF' width = '50' height = '50' viewBox = '0 0 24 24'>
-                <path d='M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z'/>
-                <path d='M0 0h24v24H0z' fill = 'none'/>
-            </svg>;
+            <svg fill='#FFFFFF' height='50' viewBox='0 0 24 24' width='50'>
+	             <path d='M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z'/>
+	             <path d='M0 0h24v24H0z' fill='none'/>
+            </svg>
         </button>
       </div>
     )
@@ -171,23 +171,19 @@ class Slider extends React.Component {
   render() {
     return(
       <div className="slider">
-        <div
-        className="wrapper"
-        style={this.setSliderStyles()}>
-        {this.render.slides()}
+        <div className="wrapper" style={this.setSliderStyles()}>
+          {this.renderSlides()}
         </div>
 
         {this.renderArrows()}
 
         <ul className="dots-container">
           {this.renderDots()}
-          </ul>
+        </ul>
 
-          <a
-          className="toggle-play"
-          onClick={this.toggleAutoplay}>
+        <a className="toggle-play" onClick={this.toggleAutoPlay}>
           {this.renderPlayStop()}
-          </a>
+        </a>
       </div>
     )
   }
